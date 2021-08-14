01c6d559e67981dfd1cc4ea7ae65a55734665955
fredomontero <christian.montero@uabc.edu.mx>
2021-08-14 12:43:09 -0700

    Added navbar, login and signup components

---
+++ src/views/Welcome.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="showLogin">
+    <Login/>
+  </div>
+  <div v-else>
+    <Signup/>
+  </div>
+</template>
+
+<script>
+import Login from '../components/Login';
+import Signup from '../components/Signup';
+export default {
+  data(){
+    return {
+      showLogin: true
+    }
+  },
+  components: {
+    Login,
+    Signup
+  }
+}
+</script>
+
+<style>
+
+</style>
